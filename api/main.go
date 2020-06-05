e66211ae73296a573764c8c86a4093142508fce1
Elwin Stephan <elwin.stephan@gmail.com>
2020-06-05 23:27:19 +0200

    move to bootstrap

--- api/main.go
+++ api/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
@@ -12,8 +13,8 @@ import (
 )
 
 func main() {
-	buffer := NewStringBuffer()
-	users := make([]int, 0)
+	buffer := NewStringBuffer(51)
+	users := make([]string, 0)
 
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -32,17 +33,16 @@ func main() {
 		}
 		defer ws.Close()
 
-		id := rand.Int()
-		users = append(users, id)
-		if err := handleClient(ws, buffer, id); err != nil {
+		username := generateName()
+		users = append(users, username)
+		if err := handleClient(ws, buffer, username); err != nil {
 			c.Logger().Error(err)
 		}
 		for i, user := range users {
-			if user == id {
+			if user == username {
 				users = append(users[:i], users[i+1:]...)
 			}
 		}
-
 
 		return nil
 	})
@@ -54,17 +54,27 @@ func main() {
 	log.Fatal(e.Start(":8888"))
 }
 
-func handleClient(ws *websocket.Conn, buffer *StringBuffer, id int) error {
+func handleClient(ws *websocket.Conn, buffer *StringBuffer, username string) error {
 	for _, msg := range buffer.Slice() {
-		if err := ws.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+		out, err := json.Marshal(msg)
+		if err != nil {
+			return err
+		}
+
+		if err := ws.WriteMessage(websocket.TextMessage, out); err != nil {
 			return err
 		}
 	}
 
-	buffer.Register(id, func(msg string) error {
-		return ws.WriteMessage(websocket.TextMessage, []byte(msg))
+	buffer.Register(username, func(msg Message) error {
+		out, err := json.Marshal(msg)
+		if err != nil {
+			return err
+		}
+
+		return ws.WriteMessage(websocket.TextMessage, out)
 	})
-	defer buffer.Unregister(id)
+	defer buffer.Unregister(username)
 
 	for {
 		// Read
@@ -74,32 +84,51 @@ func handleClient(ws *websocket.Conn, buffer *StringBuffer, id int) error {
 		} else if err != nil {
 			return err
 		}
-		buffer.Write(string(msg))
+
+		buffer.Write(Message{
+			Body:      string(msg),
+			Username:  username,
+			Timestamp: time.Now(),
+		})
 	}
+}
+
+type Message struct {
+	Body      string    `json:"body"`
+	Username  string    `json:"username"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 type StringBuffer struct {
-	msg         []string
-	subscribers map[int]func(string) error
+	start, stop int
+	msg         []Message
+	subscribers map[string]func(Message) error
 }
 
-func NewStringBuffer() *StringBuffer {
+func NewStringBuffer(n int) *StringBuffer {
 	return &StringBuffer{
-		msg:         make([]string, 0),
-		subscribers: map[int]func(string) error{},
+		start:       0,
+		stop:        0,
+		msg:         make([]Message, n),
+		subscribers: map[string]func(Message) error{},
 	}
 }
 
-func (b *StringBuffer) Register(id int, f func(string) error) {
-	b.subscribers[id] = f
+func (b *StringBuffer) Register(username string, f func(Message) error) {
+	b.subscribers[username] = f
 }
 
-func (b *StringBuffer) Unregister(id int) {
-	delete(b.subscribers, id)
+func (b *StringBuffer) Unregister(username string) {
+	delete(b.subscribers, username)
 }
 
-func (b *StringBuffer) Write(msg string) {
-	b.msg = append(b.msg, msg)
+func (b *StringBuffer) Write(msg Message) {
+	b.msg[b.stop] = msg
+	b.stop = (b.stop + 1) % len(b.msg)
+	if b.start == b.stop {
+		b.start = (b.start + 1) % len(b.msg)
+	}
+
 	for _, handler := range b.subscribers {
 		if err := handler(msg); err != nil {
 			fmt.Println(err)
@@ -107,6 +136,11 @@ func (b *StringBuffer) Write(msg string) {
 	}
 }
 
-func (b *StringBuffer) Slice() []string {
-	return b.msg
+func (b *StringBuffer) Slice() []Message {
+	out := make([]Message, 0)
+	for i := b.start; i != b.stop; i = (i + 1) % len(b.msg) {
+		out = append(out, b.msg[i])
+	}
+
+	return out
 }
