4711019bdf5a84196f8e28ccf0bd8ff74ae8d9f3
Elwin Stephan <elwin.stephan@gmail.com>
2020-06-07 20:52:52 +0200

    nicer chatview

--- ui/src/index.js
+++ ui/src/index.js
@@ -28,8 +28,6 @@ class App extends React.Component {
 
     render() {
         let messages = this.state.messages
-            .slice()
-            .reverse()
             .reduce((acc, curr) => {
                 if (acc.length === 0 || acc[acc.length - 1].username !== curr.username) {
                     return acc.concat({username: curr.username, bodies: [curr.body]});
@@ -43,7 +41,11 @@ class App extends React.Component {
                 <div className="row">
                     <div className="col-5 col-sm-3 col-lg-2"><small className="text-muted">{value.username}</small>
                     </div>
-                    <div className="col">{value.bodies.map((value, key) => <p className="mb-0" key={key}>{value}</p>)}</div>
+                    <div className="col">{value.bodies.map((value, key) => <p className="mb-0"
+                                                                              ref={(el) => {
+                                                                                  this.bottom = el;
+                                                                              }}
+                                                                              key={key}>{value}</p>)}</div>
                 </div>
             </li>);
 
@@ -52,26 +54,28 @@ class App extends React.Component {
             status = <span className="badge badge-danger">disconnected</span>;
         }
 
-        return <div className="container my-5">
-            <div className="d-flex flex-md-row flex-column-reverse justify-content-between align-items-center mb-4">
-                <form onSubmit={event => this.handleSubmit(event)} className="mb-3">
+        return <div className="d-flex flex-column container" style={{height: '100vh', overflow: 'hidden'}}>
+
+            <div className="my-3 text-right">{status}</div>
+
+            <div className="card mb-4" style={{overflow: 'hidden'}}>
+                <ul className="list-group list-group-flush">
+                    {messages}
+                </ul>
+            </div>
+
+            <div className="mb-4">
+                <form onSubmit={event => this.handleSubmit(event)}>
                     <div className="d-flex">
                         <input type="text"
                                value={this.state.value}
                                onChange={event => this.setState({value: event.target.value})}
                                className="form-control"/>
 
-                        <button type="submit" className="btn btn-primary ml-3" disabled={!this.state.connected}>Send</button>
+                        <button type="submit" className="btn btn-primary ml-3" disabled={!this.state.connected}>Send
+                        </button>
                     </div>
                 </form>
-
-                <div className="mb-3">{status}</div>
-            </div>
-
-            <div className="card">
-                <ul className="list-group list-group-flush">
-                    {messages}
-                </ul>
             </div>
         </div>
     }
@@ -86,6 +90,7 @@ class App extends React.Component {
         this.ws.onmessage = event => {
             let message = JSON.parse(event.data)
             this.setState({messages: this.state.messages.concat(message)})
+            this.bottom.scrollIntoView({behavior: 'smooth'})
         };
 
         this.ws.onclose = () => {
